112362d2c01ffd1f05436efce0e844aa265219dd
Michel <michel@mkweblinks>
2022-10-03 21:29:41 +0100

    initiation installation next js argon dashboard

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+const withSass = require("@zeit/next-sass");
+const withCSS = require("@zeit/next-css");
+const withFonts = require("next-fonts");
+const webpack = require("webpack");
+const path = require("path");
+
 /**
  *  @type {import('next').NextConfig} 
  **/
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -10,4 +19,23 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const argonConfig = withFonts(
+  withCSS(
+    withImages(
+      withSass({
+        webpack(config, options) {
+          config.module.rules.push({
+            test: /\.(eot|ttf|woff|woff2)$/,
+            use: {
+              loader: "url-loader",
+            },
+          });
+          config.resolve.modules.push(path.resolve("./"));
+          return config;
+        },
+      })
+    )
+  )
+)
+
+module.exports = argonConfig
