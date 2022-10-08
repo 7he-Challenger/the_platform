7ff64e32eca0a765b2c476858d57bec0b8e304e8
Michel HaiRun <michel.rak@hairun-technology.com>
2022-10-08 15:42:24 +0300

    config axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,16 +1,30 @@
 import Axios from 'axios';
+import COOKIES_KEY from '~constantes/cookies';
+import { getCookiesData } from '~lib/cookies';
 
 /**
  * configuration axios if neede
  * change config depending on how using axios and credential
  */
+
+const token = getCookiesData(COOKIES_KEY.TOKEN_SESSION)
+
 const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     Accept: 'application/json',
 		'Content-type': 'application/json',
+    Authorization: `Bearer ${token}`
   },
-  withCredentials: true
+  // withCredentials: true
 })
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    console.log('error axios', error)
+    return Promise.reject(error)
+  }
+)
 
 export default axios
