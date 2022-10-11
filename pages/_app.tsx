63cbd186f8ef9ab4052972b0f836177b7af9f59b
Michel HaiRun <michel.rak@hairun-technology.com>
2022-10-11 15:58:51 +0300

    init calendar plugin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '~assets/styles/globals.scss'
 import '~assets/styles/loader.scss'
+import "@fullcalendar/common/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
 // Next.js allows you to import CSS directly in .js files.
 // It handles optimization and all the necessary Webpack configuration to make this work.
 import { config } from '@fortawesome/fontawesome-svg-core'
--- pages/dashboard/emploi-du-temps/index.tsx
+++ pages/dashboard/emploi-du-temps/index.tsx
@@ -1,10 +1,40 @@
 import { AdminLayout } from '~layout'
 import type { NextPage } from 'next'
 
+import FullCalendar from "@fullcalendar/react";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import dayGridPlugin from '@fullcalendar/daygrid' 
+import { useRef } from "react";
+import styles from '~assets/styles/Calendar.module.css'
+
 const EmploiDuTemps: NextPage = () => {
+  const calendarRef = useRef<any>(null);
   return (
     <AdminLayout>
       <span>emploi du temps</span>
+      <div className={styles.container}>
+        <FullCalendar
+          // innerRef={calendarRef}
+          plugins={[
+            timeGridPlugin, 
+            interactionPlugin, 
+            dayGridPlugin
+          ]}
+          editable
+          selectable
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+          }}
+          initialView="dayGridMonth"
+          dateClick={(info) => {
+            console.log('info', info)
+          }}
+          height={'auto'}
+        />
+      </div>
     </AdminLayout>
   )
 }
