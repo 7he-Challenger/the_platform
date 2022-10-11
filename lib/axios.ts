220f72d5de206867dd008c60b90c04216a69744f
Michel HaiRun <michel.rak@hairun-technology.com>
2022-10-11 11:18:02 +0300

    [fix] change config axios logics

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,34 +1,33 @@
 import Axios from 'axios';
-import COOKIES_KEY from '~constantes/cookies';
-import { getCookiesData } from '~lib/cookies';
-
 /**
  * configuration axios if neede
  * change config depending on how using axios and credential
  */
 
-const token = getCookiesData(COOKIES_KEY.TOKEN_SESSION)
+const axiosInstance = (token = null) => {
+  const option = token ? {
+    Accept: 'application/json',
+    'Content-type': 'application/json',
+    Authorization: `Bearer ${token}`
+  } : {
+    Accept: 'application/json'
+  }
 
-const option = token ? {
-  Accept: 'application/json',
-  'Content-type': 'application/json',
-  Authorization: `Bearer ${token}`
-} : {
-  Accept: 'application/json'
+  const axios = Axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: option,
+    // withCredentials: true
+  })
+
+  axios.interceptors.response.use(
+    response => response,
+    error => {
+      console.log('error axios', error)
+      return Promise.reject(error)
+    }
+  )
+
+  return axios
 }
 
-const axios = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: option,
-  // withCredentials: true
-})
-
-axios.interceptors.response.use(
-  response => response,
-  error => {
-    console.log('error axios', error)
-    return Promise.reject(error)
-  }
-)
-
-export default axios
+export default axiosInstance
