0dc920382919b6f7a75bd34808b55daa0d969652
Michel HaiRun <michel.rak@hairun-technology.com>
2022-10-11 15:14:58 +0300

    init emploi du temps page

---
+++ pages/dashboard/emploi-du-temps/index.tsx
@@ -0,0 +1,12 @@
+import { AdminLayout } from '~layout'
+import type { NextPage } from 'next'
+
+const EmploiDuTemps: NextPage = () => {
+  return (
+    <AdminLayout>
+      <span>emploi du temps</span>
+    </AdminLayout>
+  )
+}
+
+export default EmploiDuTemps
