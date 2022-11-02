5b956658e05e7e13d3754180d7862f3c21ad2eda
Christiano David <rchristiano@isasolutions.mg>
2022-11-02 18:01:01 +0300

    formdata & initiate update user

--- models/user.ts
+++ models/user.ts
@@ -1,4 +1,23 @@
 export interface User {
   id: any;
   username: string;
+  firstname: string
+  lastname: string;
+
+}
+
+
+export type TUserPayload = {
+  username?: string
+  firstname?: string
+  lastname?: string
+  userInfo?: {
+    address?: string
+    phone?: string
+    photos?: string
+  }
+  password?: string
+  userType?: number
+  role?: number
+  cover?: string
 }
